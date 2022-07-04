a25316593fdd273bf3c69109e740ad52e0f25e52
Irene <iavivas3@gmail.com>
2022-07-04 19:08:50 +0200

    Create entity of user

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,30 @@
-export class User {}
+import { Schema, SchemaTypes, Types } from 'mongoose';
+
+const isEmail = (email: string) => {
+    const regEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return regEx.test(email);
+};
+
+export const user = new Schema({
+    name: { type: String, required: true },
+    email: {
+        type: String,
+        required: true,
+        validate: [isEmail, 'Invalid email'],
+        unique: true,
+    },
+    password: { type: String, minlength: 5 },
+    role: String,
+    players: [{ types: SchemaTypes.ObjectId, ref: 'Character' }],
+    games: [{ types: SchemaTypes.ObjectId, ref: 'Game' }],
+});
+
+export interface iUser {
+    id?: Types.ObjectId;
+    name: string;
+    email: string;
+    password: string;
+    players: Array<Types.ObjectId>;
+    games: Array<Types.ObjectId>;
+}
+
