62b45c1a279c32b75adafb8e117806022f1d4993
Irene <iavivas3@gmail.com>
2022-07-04 19:51:26 +0200

    Add create-user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,8 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+    name: string;
+    email: string;
+    password: string;
+    role: string;
+    players: Array<string>;
+    games: Array<string>;
+}
