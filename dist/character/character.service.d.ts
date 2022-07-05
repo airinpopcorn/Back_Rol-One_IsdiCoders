6d72309e8a2d2308548c60f19f60b19896d617c4
Irene Alonso <103415944+airinpopcorn@users.noreply.github.com>
2022-07-05 11:51:49 +0200

    Delete dist directory

--- dist/character/character.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { CreateCharacterDto } from './dto/create-character.dto';
-import { UpdateCharacterDto } from './dto/update-character.dto';
-export declare class CharacterService {
-    create(createCharacterDto: CreateCharacterDto): string;
-    findAll(): string;
-    findOne(id: number): string;
-    update(id: number, updateCharacterDto: UpdateCharacterDto): string;
-    remove(id: number): string;
-}
--- dist/game/dto/create-game.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateGameDto = void 0;
-class CreateGameDto {
-}
-exports.CreateGameDto = CreateGameDto;
-//# sourceMappingURL=create-game.dto.js.map
