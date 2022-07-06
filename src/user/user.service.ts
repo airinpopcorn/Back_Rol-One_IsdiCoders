644154d45c2e59b14d3cd3d6f91e9387b092375f
Irene <iavivas3@gmail.com>
2022-07-06 19:08:00 +0200

    Add login and loginWithToken methods in user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,16 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import {
+    Injectable,
+    NotFoundException,
+    UnauthorizedException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { iUser } from './entities/user.entity';
+import { AuthService } from '../auth/auth.service';
+import { BcryptService } from '../auth/bcrypt.service';
+import { JwtPayload } from 'jsonwebtoken';
 
+AuthService;
 @Injectable()
 export class UserService {
-    constructor(@InjectModel('User') private readonly User: Model<iUser>) {}
+    constructor(
+        @InjectModel('User') private readonly User: Model<iUser>,
+        private readonly auth: AuthService,
+        private readonly bcrypt: BcryptService
+    ) {}
     async create(createUserDto: CreateUserDto) {
-        const newUser = await this.User.create(createUserDto);
-        return newUser;
+        const newUser = await this.User.create({
+            ...createUserDto,
+            password: this.bcrypt.encrypt(createUserDto.password),
+        });
+        const token = this.auth.createToken(newUser.id);
+        return { user: newUser, token };
+    }
+
+    async login(loginData: { email: string; password: string }) {
+        const user = await this.User.findOne({ email: loginData.email });
+        if (
+            user === null ||
+            !this.bcrypt.compare(loginData.password, user.password)
+        )
+            throw new UnauthorizedException('Password or email not correct');
+        const token = this.auth.createToken(user.id);
+        return {
+            user,
+            token,
+        };
+    }
+
+    async loginWithToken(token: string) {
+        try {
+            const tokenData = this.auth.validateToken(
+                token.substring(7)
+            ) as JwtPayload;
+            if (typeof tokenData === 'string')
+                throw new UnauthorizedException();
+            const user = await this.User.findById(tokenData.id);
+            if (!user) throw new NotFoundException('User does not exist');
+            const newToken = this.auth.createToken(user.id);
+            return {
+                user,
+                token: newToken,
+            };
+        } catch (error) {
+            throw new UnauthorizedException('Session expired');
+        }
     }
 
     async findAll() {
