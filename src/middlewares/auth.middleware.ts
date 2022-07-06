31e8229e973645fd768693c6071b00265bcce1f4
Irene <iavivas3@gmail.com>
2022-07-06 19:09:39 +0200

    Add auth middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,26 @@
+import {
+    Injectable,
+    NestMiddleware,
+    UnauthorizedException,
+} from '@nestjs/common';
+import { JwtPayload } from 'jsonwebtoken';
+import { AuthService } from '../auth/auth.service';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+    constructor(private readonly auth: AuthService) {}
+    use(req: any, res: any, next: () => void) {
+        const token = req.get('Authorization');
+        if (!token) throw new UnauthorizedException("Token doesn't exist");
+        let tokenData: string | JwtPayload;
+        try {
+            tokenData = this.auth.validateToken(token.substring(7));
+            console.log(tokenData, 'hola');
+        } catch (error) {
+            throw new UnauthorizedException('Session expired');
+        }
+        if (typeof tokenData === 'string')
+            throw new UnauthorizedException('Token invalid');
+        next();
+    }
+}
