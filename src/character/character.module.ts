367775f0f0e35dbd5205775d94a2e087b1a82c71
Irene <iavivas3@gmail.com>
2022-07-07 18:53:01 +0200

    Add imports of game and characters in its modules

--- src/character/character.module.ts
+++ src/character/character.module.ts
@@ -4,11 +4,13 @@ import { CharacterService } from './character.service';
 import { CharacterController } from './character.controller';
 import { characterSchema } from './entities/character.entity';
 import { MongooseModule } from '@nestjs/mongoose';
+import { gameSchema } from 'src/game/entities/game.entity';
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             { name: 'Character', schema: characterSchema },
+            { name: 'Game', schema: gameSchema },
         ]),
     ],
     controllers: [CharacterController],
